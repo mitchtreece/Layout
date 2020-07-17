b7b7d5e6a2f7c10d2492a0b21c8cffb94844d9ba
Mitch Treece <mitchtreece@me.com>
2020-07-17 16:01:50 -0700

    Initial commit

---
+++ Example/Layout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Layout
+//
+//  Created by Mitch Treece on 07/17/2020.
+//  Copyright (c) 2020 Mitch Treece. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
